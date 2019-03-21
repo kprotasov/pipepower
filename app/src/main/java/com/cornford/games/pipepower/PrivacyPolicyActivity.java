3fb31dd37f6b2a5a07c2249394d26b69dc1b2493
kprotasov <kkprotasov@gmail.com>
2019-03-21 15:29:25 +0700

    Обновил версию Gradle, добавил crashlytics

--- app/src/main/java/com/cornford/games/pipepower/PrivacyPolicyActivity.java
+++ app/src/main/java/com/cornford/games/pipepower/PrivacyPolicyActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//import com.crashlytics.android.Crashlytics;
+//import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by kprotasov on 14.03.2017.
@@ -15,6 +17,7 @@ public class PrivacyPolicyActivity extends Activity {
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Fabric.with(this, new Crashlytics());
 
         if (PrivacyPolicyStore.isAccepted(this)) {
             showMainActivity();
